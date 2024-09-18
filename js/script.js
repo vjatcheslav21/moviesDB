bf1133dedda8a271024a1168caf7e86a000ef774
Slava Kulikov <cornelita@yandex.ru>
2024-09-18 13:52:20 +0300

    В этом коммите изменена структура сайта. Приложение теперь не консольное, а имеет функционал доступный пользователю. Выполнены базовые задачи по взаимодействию с элементами сайта.

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const asideAdd = document.querySelector('.promo__adv');
+const genre = document.querySelector('.promo__genre');
+const bgImg = document.querySelector('.promo__bg');
+const movieList = document.querySelector('.promo__interactive-list');
+
+asideAdd.remove();
+
+
+genre.innerHTML = 'драмма';
+
+bgImg.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `
+})
+
+
